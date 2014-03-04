e381aea0a2c05067d532a9d3b96c47a63075d1b1
Jon Saxton <tech-coordinator@saxton-solutions.com>
2014-03-04 15:42:16 -0700

    Fixed version number format.

--- cli/update.php
+++ cli/update.php
@@ -36,7 +36,7 @@ if (!empty($settings)) //Sanitation check! make sure we have a dbtable set for '
   {
     fwrite(STDOUT,"Your database is out of date. You cannot proceed beyond this point without an update! Apply update now? [Y]es or [N]o ");
     $update=trim(fgets(STDIN),"\n");
-    $version=1.1;
+    $version="1.1";
   }
 }
 else
@@ -49,7 +49,7 @@ else
 if (strtolower($update) == 'y')
 {
   require '../assets/core/install.inc.php'; //Update function resides in this file for global interface use
-  if ($version == 1.1)
+  if ($version == "1.1")
   {
    fwrite(STDOUT,"MomoKO 1.5 moved a few settings to the database and introduced other new setting values for better and easier customization. I will now begin asking you a series of questions so you may set these new values. Just like when you first installed MomoKO, if you see brackets [], it means I can fill in the default value presented within if you leave the answer blank. Otherwise all questions are required!\n");
    fwrite(STDOUT,"What was the name of your site as you configured it in main.conf.txt for MomoKO 1.1 or earlier?");
