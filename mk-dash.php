33279e8c845fe703bd5b76784987fac086cf8e42
Jon Saxton <tech-coordinator@saxton-solutions.com>
2015-03-25 10:26:51 -0600

    Fixed uri for log out redirection on dashboard. Users can now logout from there! See issue #4

--- mk-dash.php
+++ mk-dash.php
@@ -1106,7 +1106,7 @@ HTML;
 
 if (@$_GET['action'] == 'login' || @$_GET['action'] == 'logout' || @$_GET['action'] == 'register')
 {
- header("Location: //".$GLOBALS['CFG']->domain.$GLOBALS['CFG']->location."?action=".$_GET['action']);
+ header("Location: //".$GLOBALS['SET']['baseuri']."?action=".$_GET['action']);
 }
 else
 {
