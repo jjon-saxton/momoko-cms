c9c2723fd36f7142af311b77c549565a61539a1c
jjon_saxton <jjon.saxton@gmail.com>
2015-04-14 15:55:07 +0000

    Found more URLs to update, and fixed issue #18

--- ajax.php
+++ ajax.php
@@ -1,6 +1,6 @@
 <?php
-require_once dirname(__FILE__).'/assets/core/common.inc.php';
-require_once dirname(__FILE__).'/assets/core/content.inc.php';
+require_once dirname(__FILE__).'/core/common.inc.php';
+require_once dirname(__FILE__).'/core/content.inc.php';
 
 include dirname(__FILE__).'/core/'.$_GET['include'].'.inc.php';
 
