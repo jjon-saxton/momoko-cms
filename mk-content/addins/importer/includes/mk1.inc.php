421975a853c1eace370b016f4f40f0648d129641
Jon Saxton <cem@tower21studios.com>
2016-02-29 13:40:03 -0700

    Added a sanity check for unique file names due to flat attachment hiarchy in 2.1.

--- mk-content/addins/importer/includes/mk1.inc.php
+++ mk-content/addins/importer/includes/mk1.inc.php
@@ -76,20 +76,39 @@ function add_files_r($folder)
         }
         else
         {
-            $file['title']=$item;
-            $file['type']="file";
-            $file['status']="public"
-            $file['author']=1;
-            //TODO find mime type for $file['mime_type']
-            //TODO decide if we should set $file['parent']
-            $file['link']=$GLOBALS['SET']['baseuri'].$GLOBALS['SET']['filedir'].$item;
+            if (!file_exists($GLOBALS['SET']['baseuri'].$GLOBALS['SET']['filedir'].$item)) // all attachments are store in the filedir, there is no hiarchy there for mk2, as such we need to make file names unique to avoid overwrites
+            {
+                $file['title']=$item;
+            }
+            else
+            {
+                $file['title']=time().$item;
+            }
+            $file['type']="attachment";
+            $file['status']="public";
+            $file['date_created']=date("Y-m-d H:i:s");
+            $file['author']=$GLOBALS['USR']->num;
+            if (class_exists('finfo'))
+            {
+                $finfo=new finfo(FILEINFO_MIME_TYPE);
+                $file['mime_type']=$finfo->file($folder.$item);
+            }
+            else
+            {
+                trigger_error("Could not determine mime type of an imported file! finfo class does not exist, so mime type set is not specific. Recommend updating PHP or installing the fileinfo PECL extension to allow for proper mime type settings.",E_USER_NOTICE);
+                $finfo['mime_type']="application/octet-stream";
+            }
+            $file['parent']=0; //since files can only be assigned to pages and 1.x did not support his, we will set all files to root
+            $file['link']=$GLOBALS['SET']['baseuri'].$GLOBALS['SET']['filedir'].$file['title'];
 
             if ($items[]=$content->putData($files))
             {
-                move($folder.$item,$GLOBALS['SET']['basedir'].$GLOBALS['SET']['filedir'].$item);
+                rename($folder.$item,$GLOBALS['SET']['basedir'].$GLOBALS['SET']['filedir'].$file['title']);
             }
         }
     }
+
+    return $items;
 }
 
 function add_posts($xml)
